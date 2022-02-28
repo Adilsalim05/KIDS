8c19fd098325c6e2dae04ae5137b2e46e729bdd6
Adilsalim05 <80718054+Adilsalim05@users.noreply.github.com>
2022-02-28 10:59:20 -0800

    Update data_manager.py

--- kg_constructor/integrate_modules/data_manager.py
+++ kg_constructor/integrate_modules/data_manager.py
@@ -30,8 +30,8 @@ class DataManager:
  #   Class for managing the data.
     """
     
-    def __init__(self, data_paths, map_file=None, data_rule_file=None, replace_rule_file=None):
-        """
+   # def __init__(self, data_paths, map_file=None, data_rule_file=None, replace_rule_file=None):
+    """
 """        Class constructor for DataManager. All inputs are optional
         since only name mapping may be used somewhere else.
 
