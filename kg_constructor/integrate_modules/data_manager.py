00aa73017acffccba85ac8d032f3523ece40f696
Adilsalim05 <80718054+Adilsalim05@users.noreply.github.com>
2022-02-28 11:24:48 -0800

    Update data_manager.py

--- kg_constructor/integrate_modules/data_manager.py
+++ kg_constructor/integrate_modules/data_manager.py
@@ -144,9 +144,9 @@ def infer(self, pd_data):
         Returns:
             pd_updated: (pd.DataFrame) Data with new inferred data added.
         """
-    if not self.data_rule_file:
-        log.info('Data rule file not specified. Skipping knowledge inferral...')
-        return pd_data
+        if not self.data_rule_file:
+            log.info('Data rule file not specified. Skipping knowledge inferral...')
+            return pd_data
 
     data_rules = ET.parse(self.data_rule_file).getroot()
     pd_updated = pd_data.copy()
