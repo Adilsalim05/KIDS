5933d0e61da617027436bdcb9535c663a861f7a8
Adilsalim05 <80718054+Adilsalim05@users.noreply.github.com>
2022-03-18 04:44:29 -0700

    Update config.sh

--- hypothesis_generator/pra/model/model_instance/fold_0/config.sh
+++ hypothesis_generator/pra/model/model_instance/fold_0/config.sh
@@ -5,7 +5,7 @@ start_relation="confers#SPACE#resistance#SPACE#to#SPACE#antibiotic"
 
 # The path to the directory of your data
 
-#DATA_PATH="/path/to/data/directory/kb/folds/fold_0"
+DATA_PATH="/path/to/data/directory/kb/folds/fold_0"
 DATA_PATH ="/Users/adilsalim/KIDS/hypothesis_generator/data/kb/folds/fold_0"
 
 
