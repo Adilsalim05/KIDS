afff58038c0f6777372cfd4399d012e18234ded8
Muhammad Adil Salim <adilsalim@Muhammads-MacBook-Pro.local>
2022-03-18 04:52:01 -0700

    Data path reverted line 7

--- hypothesis_generator/pra/model/model_instance/fold_0/config.sh
+++ hypothesis_generator/pra/model/model_instance/fold_0/config.sh
@@ -4,10 +4,8 @@
 start_relation="confers#SPACE#resistance#SPACE#to#SPACE#antibiotic"
 
 # The path to the directory of your data
-
 DATA_PATH="/path/to/data/directory/kb/folds/fold_0"
-DATA_PATH ="/Users/adilsalim/KIDS/hypothesis_generator/data/kb/folds/fold_0"
-
+#DATA_PATH ="/Users/adilsalim/KIDS/kg_constructor/output/folds/fold_0"
 
 # This is deprecated
 use_negatives=false
