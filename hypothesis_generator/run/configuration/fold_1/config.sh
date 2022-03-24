1c06f9d5415918a4d9598a26c0051f6d9080c13b
Adilsalim05 <80718054+Adilsalim05@users.noreply.github.com>
2022-03-23 21:09:19 -0700

    Update config.sh

--- hypothesis_generator/run/configuration/fold_1/config.sh
+++ hypothesis_generator/run/configuration/fold_1/config.sh
@@ -4,8 +4,8 @@
 start_relation="confers#SPACE#resistance#SPACE#to#SPACE#antibiotic"
 
 # The path to the directory of your data
-#DATA_PATH="/home/asalim/KIDS/hypothesis_generator/data/kb/folds/fold_1"
-DATA_PATH = "/Users/adilsalim/KIDS/kg_constructor/data/kb/final"
+DATA_PATH="/home/asalim/KIDS/hypothesis_generator/data/kb/folds/fold_1"
+#DATA_PATH = "/Users/adilsalim/KIDS/kg_constructor/data/kb/final"
 
 # This is deprecated
 use_negatives=false
