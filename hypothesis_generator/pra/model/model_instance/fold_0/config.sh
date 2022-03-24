e7f7599370bc02f9d45ce4ba5a54b7e8b825b98d
Adilsalim05 <asalim@ucdavis.edu>
2022-03-23 20:54:53 -0700

    update

--- hypothesis_generator/pra/model/model_instance/fold_0/config.sh
+++ hypothesis_generator/pra/model/model_instance/fold_0/config.sh
@@ -4,8 +4,8 @@
 start_relation="confers#SPACE#resistance#SPACE#to#SPACE#antibiotic"
 
 # The path to the directory of your data
-DATA_PATH="/home/asalim/KIDS/hypothesis_generator/data/kb/folds/fold_0"
-
+#DATA_PATH="/home/asalim/KIDS/hypothesis_generator/data/kb/folds/fold_0"
+DATA_PATH ="/Users/adilsalim/KIDS/kg_constructor/output/folds/fold_0"
 
 # This is deprecated
 use_negatives=false
@@ -21,5 +21,3 @@ train_file="train.txt"
 
 # The name of the file that will be predicted on.
 predict_file=train_local.txt
-
-#DATA_PATH ="/Users/adilsalim/KIDS/kg_constructor/output/folds/fold_0"
